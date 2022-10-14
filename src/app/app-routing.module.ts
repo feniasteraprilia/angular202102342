d395c6453857c7645cc475741189ceeec15626b2
feniasteraprilia <113622748+feniasteraprilia@users.noreply.github.com>
2022-10-14 15:34:54 +0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: "admin", component: AdminComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "", redirectTo: "login", pathMatch: "full"},
+  { path: "", redirectTo: "register", pathMatch: "full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
